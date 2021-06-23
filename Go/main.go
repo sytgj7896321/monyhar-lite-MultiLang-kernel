07c3830c019bd7f3b1b40e484d00868d6c09ea9d
sytgj7896321 <70554029+sytgj7896321@users.noreply.github.com>
2021-06-24 00:54:56 +0800

    Update main.go

--- Go/main.go
+++ Go/main.go
@@ -1,8 +1,30 @@
-func surf_internet(obj int) int {
+package main
 
-   var result int
-  
-   return 0
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+var commands = map[string]string{
+	"windows": "cmd",
+	"darwin":  "open",
+	"linux":   "xdg-open",
 }
 
-return surf_internet()
+// open calls the OS default program for url
+func open(url string) error {
+	run, ok := commands[runtime.GOOS]
+	if !ok {
+		return fmt.Errorf("don't know how to open things on %s platform", runtime.GOOS)
+	}
+	if run == "cmd" {
+		return exec.Command(run, "/c", "start", url).Start()
+	} else {
+		return exec.Command(run, url).Start()
+	}
+}
+
+func main() {
+	_ = open("https://github.com/tucaoba2333/monyhar-lite-MultiLang-kernel")
+}
