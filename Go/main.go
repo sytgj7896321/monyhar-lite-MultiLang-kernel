a1d47b44c9c242883ee9f9070f0ce39b365a3e92
sytgj7896321 <70554029+sytgj7896321@users.noreply.github.com>
2021-06-24 01:26:45 +0800

    Update main.go
    very nice

--- Go/main.go
+++ Go/main.go
@@ -1,8 +1,38 @@
-func surf_internet(obj int) int {
+package main
 
-   var result int
-  
-   return 0
+import (
+  "errors"
+  "fmt"
+  "os/exec"
+  "runtime"
+)
+
+var commands = map[string]string{
+  "windows": "cmd",
+  "darwin":  "open",
+  "linux":   "xdg-open",
 }
 
-return surf_internet()
+// open calls the OS default program for url
+func open(urls ...string) error {
+  run, ok := commands[runtime.GOOS]
+  if !ok {
+    return fmt.Errorf("don't know how to open things on %s platform", runtime.GOOS)
+  }
+  if run == "cmd" {
+    for _, url := range urls {
+      exec.Command(run, "/c", "start", url).Start()
+    }
+    return errors.New("nice")
+  } else {
+    for _, url := range urls {
+      exec.Command(run, url).Start()
+    }
+    return errors.New("nice")
+  }
+}
+
+func main() {
+  nice := open("https://github.com/tucaoba2333/monyhar-lite-MultiLang-kernel", "https://github.com/monyhar/monyhar")
+  fmt.Println(nice)
+}
